52753254505036be9ca6105ed53ed1a8c8eb0b3b
kvsrinivas91 <43247128+kvsrinivas91@users.noreply.github.com>
2018-10-29 11:40:20 +0530

    update for rover
    includes 180 degrees

--- p_90.ino
+++ p_90.ino
@@ -57,6 +57,53 @@ void loop(){
   motor1.run(FORWARD);
   motor2.run(FORWARD);
   delay(2000);
+ 
+ delay(5000);
+  motor1.setSpeed(232);  //first straight 
+  motor2.setSpeed(250);  //change the speeds by trail and error method and make it go straight 
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  delay(4000);
+  motor1.setSpeed(250);  //right turn
+  motor2.setSpeed(232);
+  motor1.run(FORWARD);
+  motor2.run(RELEASE);
+  delay(950);              //change the delay and see that it turns exactly 90degrees by trail and error method 
+  motor1.setSpeed(232);  //small straight
+  motor2.setSpeed(250);
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  delay(2000);
+  motor1.setSpeed(250);  //right turn
+  motor2.setSpeed(232);
+  motor1.run(FORWARD);
+  motor2.run(RELEASE);
+  delay(950);
+  motor1.setSpeed(232);  //small straight
+  motor2.setSpeed(250);
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  delay(2000);
+  motor1.setSpeed(250);  //right turn
+  motor2.setSpeed(232);
+  motor1.run(FORWARD);
+  motor2.run(RELEASE);
+  delay(950);
+  motor1.setSpeed(232);  //small straight
+  motor2.setSpeed(250);
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  delay(2000);
+   motor1.setSpeed(250);  //left turn
+  motor2.setSpeed(232);
+  motor1.run(RELEASE);
+  motor2.run(FORWARD);
+  delay(950);
+   motor1.setSpeed(232);  //small straight
+  motor2.setSpeed(250);
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  delay(2000);
   
   
   motor1.run(RELEASE); // stop
